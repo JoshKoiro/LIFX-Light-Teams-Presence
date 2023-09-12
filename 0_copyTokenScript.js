4c6235d0bcfbfe7ee10f3a77b7561a2ae33e7efd
JoshKoiro <josh.koiro@gmail.com>
2023-09-12 16:44:44 -0400

    created logic to copy contents of 0_key.txt as API Key

---
+++ 0_copyTokenScript.js
@@ -0,0 +1,5 @@
+(function token(){
+	let el = document.getElementById('access-tokens-tab')
+	copy(el.textContent)
+	console.log('token is copied!')	
+})()
--- updateContainer.sh
+++ updateContainer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+node updateEnv.js
 sudo docker-compose down\
 && git pull origin main\
 && docker image rm lifx-light-teams-presence:latest\
