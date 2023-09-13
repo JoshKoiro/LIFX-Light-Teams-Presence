8a03c5c0546416279624529a3662c5b4bcbe7980
JoshKoiro <josh.koiro@gmail.com>
2023-09-12 21:13:27 -0400

    minor formatting corrections to updateContainer

--- updateContainer.sh
+++ updateContainer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 node updateEnv.js
 sudo docker-compose down\
-docker image rm lifx-light-teams-presence:latest\
+&& docker image rm lifx-light-teams-presence:latest\
 && docker build -t lifx-light-teams-presence .\
 && docker compose up -d
