2f8943a41dd1caba90a90bdca8a54a5d5a344d41
JoshKoiro <josh.koiro@gmail.com>
2023-09-12 21:11:49 -0400

    removed git pull in updateContainer.sh - updates should be done manually

--- updateContainer.sh
+++ updateContainer.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 node updateEnv.js
 sudo docker-compose down\
-&& git pull origin main\
 docker image rm lifx-light-teams-presence:latest\
 && docker build -t lifx-light-teams-presence .\
 && docker compose up -d
